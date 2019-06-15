86b911ff5a0b0e6bc2056abe242a8c7b75c7b7bb
Neil Atkinson <neil@bluespot.io>
2019-06-15 10:28:30 +0100

    Add BitmapManager#show_image method.

--- lib/bitmap_manager.rb
+++ lib/bitmap_manager.rb
@@ -1,5 +1,14 @@
 # frozen_string_literal: true
 
+require_relative 'bitmap_image'
+
 # Translates commands into transformations understood by the BitmapImage
 class BitmapManager
+  def show_image
+    current_image.to_s
+  end
+
+  private
+
+  attr_accessor :current_image
 end
--- spec/bitmap_manager_spec.rb
+++ spec/bitmap_manager_spec.rb
@@ -3,5 +3,33 @@
 require 'bitmap_manager'
 
 RSpec.describe BitmapManager do
+  let(:bitmap_manager) { described_class.new }
 
+  describe 'show_image' do
+    subject { bitmap_manager.show_image }
+
+    context 'when no current_image (by default)' do
+      it { is_expected.to eq '' }
+    end
+
+    context 'when current_image is set' do
+      let(:bitmap_image) { BitmapImage.new(3, 4) }
+
+      before do
+        # Stub private current_image value
+        allow(bitmap_manager).to receive(:current_image)
+          .and_return(bitmap_image)
+      end
+
+      it 'returns the to_s representation of the current_image' do
+        expect(subject).to eq(<<~PIXELS
+            OOO
+            OOO
+            OOO
+            OOO
+          PIXELS
+        )
+      end
+    end
+  end
 end

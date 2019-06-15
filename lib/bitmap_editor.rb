aeba7c41d27ab233e3cfdaa4acd5dd13a297618f
Neil Atkinson <neil@bluespot.io>
2019-06-15 08:24:19 +0100

    Add case statements for all specified commands.
    Just puts to terminal for now

--- lib/bitmap_editor.rb
+++ lib/bitmap_editor.rb
@@ -6,6 +6,16 @@ class BitmapEditor
     File.open(file).each do |line|
       line = line.chomp
       case line
+      when 'I'
+        puts "new image"
+      when 'C'
+        puts "clearing image"
+      when 'L'
+        puts "drawing single pixel"
+      when 'V'
+        puts "drawing vertical line"
+      when 'H'
+        puts "drawing horizontal line"
       when 'S'
         puts "There is no image"
       else

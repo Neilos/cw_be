d4c6bf15f4bbd572889f836374a2cf70cd93433a
Neil Atkinson <neil@bluespot.io>
2019-06-15 12:47:28 +0100

    Refactor bitmap_image_spec

--- spec/bitmap_image_spec.rb
+++ spec/bitmap_image_spec.rb
@@ -21,6 +21,8 @@ RSpec.describe BitmapImage do
   end
 
   describe '#clear' do
+    subject { bitmap.clear }
+
     before do
       # Fill some pixels with color
       bitmap.fill(2, 2, 'F')
@@ -30,9 +32,7 @@ RSpec.describe BitmapImage do
     end
 
     it 'resets all the pixels to their original "O" color' do
-      expect {
-        bitmap.clear
-      }.to change {
+      expect { subject }.to change {
         bitmap.to_s
       }.from(
         <<~PIXELS
@@ -58,32 +58,46 @@ RSpec.describe BitmapImage do
 
   describe '#fill' do
     context 'when position is within image bounds' do
+      subject { bitmap.fill(3, 4, 'Y') }
+
       it 'fills the specified pixel with the given color' do
-        bitmap.fill(3, 4, 'Y')
-        expect(bitmap.to_s).to eq <<~PIXELS
-          OOOOO
-          OOOOO
-          OOOOO
-          OOYOO
-          OOOOO
-          OOOOO
-        PIXELS
+        expect { subject }.to change {
+          bitmap.to_s
+        }.from(
+          <<~PIXELS
+            OOOOO
+            OOOOO
+            OOOOO
+            OOOOO
+            OOOOO
+            OOOOO
+          PIXELS
+        ).to(
+          <<~PIXELS
+            OOOOO
+            OOOOO
+            OOOOO
+            OOYOO
+            OOOOO
+            OOOOO
+          PIXELS
+        )
       end
     end
 
     context 'when position is beyond image right edge' do
+      subject { bitmap.fill(6, 4, 'Y') }
+
       it 'does nothing' do
-        expect {
-          bitmap.fill(6, 4, 'Y')
-        }.not_to change { bitmap.to_s }
+        expect { subject }.not_to change { bitmap.to_s }
       end
     end
 
     context 'when position is beyond image bottom edge' do
+      subject { bitmap.fill(3, 7, 'Y') }
+
       it 'does nothing' do
-        expect {
-          bitmap.fill(3, 7, 'Y')
-        }.not_to change { bitmap.to_s }
+        expect { subject }.not_to change { bitmap.to_s }
       end
     end
   end

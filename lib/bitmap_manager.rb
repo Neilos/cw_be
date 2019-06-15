c37713bb6c7ba2275944dcfed2c22fa51d305f6b
Neil Atkinson <neil@bluespot.io>
2019-06-15 11:24:50 +0100

    Allow for possibility that bitmap_manager has no current_image

--- lib/bitmap_manager.rb
+++ lib/bitmap_manager.rb
@@ -5,20 +5,24 @@ require_relative 'bitmap_image'
 # Translates commands into transformations understood by the BitmapImage
 class BitmapManager
   def clear_image
-    current_image.clear
+    current_image&.clear
   end
 
   def draw_single_pixel(coords_x, coords_y, color)
-    current_image.fill(coords_x, coords_y, color)
+    current_image&.fill(coords_x, coords_y, color)
   end
 
   def draw_horizontal_line(coords_x1, coords_x2, coords_y, color)
+    return unless current_image
+
     (coords_x1..coords_x2).each do |coords_x|
       current_image.fill(coords_x, coords_y, color)
     end
   end
 
   def draw_vertical_line(coords_x, coords_y1, coords_y2, color)
+    return unless current_image
+
     (coords_y1..coords_y2).each do |coords_y|
       current_image.fill(coords_x, coords_y, color)
     end
--- spec/bitmap_manager_spec.rb
+++ spec/bitmap_manager_spec.rb
@@ -6,193 +6,231 @@ RSpec.describe BitmapManager do
   let(:bitmap_manager) { described_class.new }
 
   describe 'clear_image' do
-    let(:bitmap_image) { BitmapImage.new(2, 2) }
-
-    before do
-      allow(bitmap_manager).to receive(:current_image)
-        .and_return(bitmap_image)
+    context 'when current_image is NOT present' do
+      it 'does nothing (without complaining)' do
+        expect { bitmap_manager.clear_image }.not_to raise_error
+      end
     end
 
-    it 'delegates "clear" instruction to the current_image' do
-      expect(bitmap_image).to receive(:clear)
-      bitmap_manager.clear_image
+    context 'when current_image is present' do
+      let(:bitmap_image) { BitmapImage.new(2, 2) }
+
+      before do
+        allow(bitmap_manager).to receive(:current_image)
+          .and_return(bitmap_image)
+      end
+
+      it 'delegates "clear" instruction to the current_image' do
+        expect(bitmap_image).to receive(:clear)
+        bitmap_manager.clear_image
+      end
     end
   end
 
   describe 'draw_horizontal_line' do
-    before do
-      bitmap_manager.new_image(5, 6)
-    end
-
-    context 'when line is fully within image bounds' do
-      it 'sets appropriate pixels to the given color' do
+    context 'when current_image is NOT present' do
+      it 'does nothing (without complaining)' do
         expect {
           bitmap_manager.draw_horizontal_line(3, 5, 2, 'Z')
-        }.to change {
-          bitmap_manager.show_image
-        }.from(
-          <<~PIXELS
-            OOOOO
-            OOOOO
-            OOOOO
-            OOOOO
-            OOOOO
-            OOOOO
-          PIXELS
-        ).to(
-          <<~PIXELS
-            OOOOO
-            OOZZZ
-            OOOOO
-            OOOOO
-            OOOOO
-            OOOOO
-          PIXELS
-        )
+        }.not_to raise_error
       end
     end
 
-    context 'when line is partially outside image bounds' do
-      it 'sets appropriate pixels to the given color' do
-        expect {
-          bitmap_manager.draw_horizontal_line(3, 7, 2, 'Z')
-        }.to change {
-          bitmap_manager.show_image
-        }.from(
-          <<~PIXELS
-            OOOOO
-            OOOOO
-            OOOOO
-            OOOOO
-            OOOOO
-            OOOOO
-          PIXELS
-        ).to(
-          <<~PIXELS
-            OOOOO
-            OOZZZ
-            OOOOO
-            OOOOO
-            OOOOO
-            OOOOO
-          PIXELS
-        )
+    context 'when current_image is present' do
+      before do
+        bitmap_manager.new_image(5, 6)
       end
-    end
 
-    context 'when line is completely outside image bounds' do
-      it 'sets appropriate pixels to the given color' do
-        expect {
-          bitmap_manager.draw_horizontal_line(6, 9, 2, 'Z')
-        }.not_to change { bitmap_manager.show_image }
+      context 'when line is fully within image bounds' do
+        it 'sets appropriate pixels to the given color' do
+          expect {
+            bitmap_manager.draw_horizontal_line(3, 5, 2, 'Z')
+          }.to change {
+            bitmap_manager.show_image
+          }.from(
+            <<~PIXELS
+              OOOOO
+              OOOOO
+              OOOOO
+              OOOOO
+              OOOOO
+              OOOOO
+            PIXELS
+          ).to(
+            <<~PIXELS
+              OOOOO
+              OOZZZ
+              OOOOO
+              OOOOO
+              OOOOO
+              OOOOO
+            PIXELS
+          )
+        end
+      end
+
+      context 'when line is partially outside image bounds' do
+        it 'sets appropriate pixels to the given color' do
+          expect {
+            bitmap_manager.draw_horizontal_line(3, 7, 2, 'Z')
+          }.to change {
+            bitmap_manager.show_image
+          }.from(
+            <<~PIXELS
+              OOOOO
+              OOOOO
+              OOOOO
+              OOOOO
+              OOOOO
+              OOOOO
+            PIXELS
+          ).to(
+            <<~PIXELS
+              OOOOO
+              OOZZZ
+              OOOOO
+              OOOOO
+              OOOOO
+              OOOOO
+            PIXELS
+          )
+        end
+      end
+
+      context 'when line is completely outside image bounds' do
+        it 'sets appropriate pixels to the given color' do
+          expect {
+            bitmap_manager.draw_horizontal_line(6, 9, 2, 'Z')
+          }.not_to change { bitmap_manager.show_image }
+        end
       end
     end
   end
 
   describe 'draw_single_pixel' do
-    before do
-      bitmap_manager.new_image(4, 4)
-    end
-
-    context 'when position is outside of image bounds' do
-      it 'has no effect on the current_image' do
+    context 'when current_image is NOT present' do
+      it 'does nothing (without complaining)' do
         expect {
           bitmap_manager.draw_single_pixel(2, 5, 'B')
-        }.not_to change { bitmap_manager.show_image }
+        }.not_to raise_error
       end
     end
 
-    context 'when position is within image bounds' do
-      it 'sets a specified pixel of the current_image to the given color' do
-        expect {
-          bitmap_manager.draw_single_pixel(2, 3, 'B')
-        }.to change {
-          bitmap_manager.show_image
-        }.from(
-          <<~PIXELS
-            OOOO
-            OOOO
-            OOOO
-            OOOO
-          PIXELS
-        ).to(
-          <<~PIXELS
-            OOOO
-            OOOO
-            OBOO
-            OOOO
-          PIXELS
-        )
+    context 'when current_image is present' do
+      before do
+        bitmap_manager.new_image(4, 4)
+      end
+
+      context 'when position is outside of image bounds' do
+        it 'has no effect on the current_image' do
+          expect {
+            bitmap_manager.draw_single_pixel(2, 5, 'B')
+          }.not_to change { bitmap_manager.show_image }
+        end
+      end
+
+      context 'when position is within image bounds' do
+        it 'sets a specified pixel of the current_image to the given color' do
+          expect {
+            bitmap_manager.draw_single_pixel(2, 3, 'B')
+          }.to change {
+            bitmap_manager.show_image
+          }.from(
+            <<~PIXELS
+              OOOO
+              OOOO
+              OOOO
+              OOOO
+            PIXELS
+          ).to(
+            <<~PIXELS
+              OOOO
+              OOOO
+              OBOO
+              OOOO
+            PIXELS
+          )
+        end
       end
     end
   end
 
   describe 'draw_vertical_line' do
-    before do
-      bitmap_manager.new_image(5, 6)
-    end
-
-    context 'when line is fully within image bounds' do
-      it 'sets appropriate pixels to the given color' do
+    context 'when current_image is NOT present' do
+      it 'does nothing (without complaining)' do
         expect {
           bitmap_manager.draw_vertical_line(2, 3, 6, 'W')
-        }.to change {
-          bitmap_manager.show_image
-        }.from(
-          <<~PIXELS
-            OOOOO
-            OOOOO
-            OOOOO
-            OOOOO
-            OOOOO
-            OOOOO
-          PIXELS
-        ).to(
-          <<~PIXELS
-            OOOOO
-            OOOOO
-            OWOOO
-            OWOOO
-            OWOOO
-            OWOOO
-          PIXELS
-        )
+        }.not_to raise_error
       end
     end
 
-    context 'when line is partially outside image bounds' do
-      it 'sets appropriate pixels to the given color' do
-        expect {
-          bitmap_manager.draw_vertical_line(2, 3, 10, 'W')
-        }.to change {
-          bitmap_manager.show_image
-        }.from(
-          <<~PIXELS
-            OOOOO
-            OOOOO
-            OOOOO
-            OOOOO
-            OOOOO
-            OOOOO
-          PIXELS
-        ).to(
-          <<~PIXELS
-            OOOOO
-            OOOOO
-            OWOOO
-            OWOOO
-            OWOOO
-            OWOOO
-          PIXELS
-        )
+    context 'when current_image is present' do
+      before do
+        bitmap_manager.new_image(5, 6)
       end
-    end
 
-    context 'when line is completely outside image bounds' do
-      it 'sets appropriate pixels to the given color' do
-        expect {
-          bitmap_manager.draw_vertical_line(7, 6, 10, 'W')
-        }.not_to change { bitmap_manager.show_image }
+      context 'when line is fully within image bounds' do
+        it 'sets appropriate pixels to the given color' do
+          expect {
+            bitmap_manager.draw_vertical_line(2, 3, 6, 'W')
+          }.to change {
+            bitmap_manager.show_image
+          }.from(
+            <<~PIXELS
+              OOOOO
+              OOOOO
+              OOOOO
+              OOOOO
+              OOOOO
+              OOOOO
+            PIXELS
+          ).to(
+            <<~PIXELS
+              OOOOO
+              OOOOO
+              OWOOO
+              OWOOO
+              OWOOO
+              OWOOO
+            PIXELS
+          )
+        end
+      end
+
+      context 'when line is partially outside image bounds' do
+        it 'sets appropriate pixels to the given color' do
+          expect {
+            bitmap_manager.draw_vertical_line(2, 3, 10, 'W')
+          }.to change {
+            bitmap_manager.show_image
+          }.from(
+            <<~PIXELS
+              OOOOO
+              OOOOO
+              OOOOO
+              OOOOO
+              OOOOO
+              OOOOO
+            PIXELS
+          ).to(
+            <<~PIXELS
+              OOOOO
+              OOOOO
+              OWOOO
+              OWOOO
+              OWOOO
+              OWOOO
+            PIXELS
+          )
+        end
+      end
+
+      context 'when line is completely outside image bounds' do
+        it 'sets appropriate pixels to the given color' do
+          expect {
+            bitmap_manager.draw_vertical_line(7, 6, 10, 'W')
+          }.not_to change { bitmap_manager.show_image }
+        end
       end
     end
   end
